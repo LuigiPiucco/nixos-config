70e57b675f82324d0523f0fa1259af3cc72c6cbe
Luigi Sartor Piucco <luigipiucco@gmail.com>
2024-01-13 15:25:55 -0300

    Add nix-community substituters
    To get emacs and other goodies from cache if available.

--- flake.nix
+++ flake.nix
@@ -1,10 +1,20 @@
 {
   description = "Luigi's system config.";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     home.url = "github:nix-community/home-manager";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
+
     emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
 
